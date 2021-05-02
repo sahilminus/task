1617518c796eccf82ad13159f570bc1e0dd9ea7f
Sahil Mishra <sahil.mishra@cliffex.com>
2021-05-02 09:28:09 +0530

    console

--- utils/database.js
+++ utils/database.js
@@ -8,7 +8,7 @@ mongoose.connect(process.env.MONGODB_URL, {
 })
 
 var db = mongoose.connection;
-console.log(process.env.MONGODB_URL);
+// console.log(process.env.MONGODB_URL);
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
